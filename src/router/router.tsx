bd7397b69a6e82302c83d6fa3b7366a14df48ee2
jimmy-codix <212524470+jimmy-codix@users.noreply.github.com>
2025-09-18 08:12:35 +0200

    Commented out requireAuthLoader until login feature is done

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,12 +4,15 @@ import { Login } from '../features/auth/components';
 import { requireAuthLoader } from '../features/auth/loaders';
 import { Companies, Company } from '../features/companies/components';
 import { companiesLoader, companyLoader } from '../features/companies/loaders';
+import {TestArea} from '../features/testarea';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       {/* requireAuthLoader is a route guard that protects the App and its child routes. */}
-      <Route element={<App />} loader={requireAuthLoader} path="/">
+      {/* TODO uncomment line below and remove "<Route element={<App />} path="/"></Route>" when front end login is done */}
+      {/* <Route element={<App />} loader={requireAuthLoader} path="/"> */}
+      <Route element={<App />} path="/">
         <Route element={<Companies />} index loader={companiesLoader} />
         <Route
           element={<Company />}
@@ -20,6 +23,8 @@ export const router = createBrowserRouter(
         />
       </Route>
       <Route element={<Login />} path="/login" />
+      {/* TODO: remove this in a production enviroment TestArea */}
+      <Route element={<TestArea />} path="/testarea" />
     </>
   )
 );
