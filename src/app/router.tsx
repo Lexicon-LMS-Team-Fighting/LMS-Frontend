1289ef38a4c5b5d0d801a4281e0856f398797119
Bosseyang <bosse_yang@hotmail.com>
2025-09-23 12:26:35 +0200

    update router to temporarily include ModuleList component for development

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
-import { App } from '.';
-import { Login } from '../pages/login';
-import { requireAuthLoader } from '../features/auth/loaders';
-import { Companies, Company } from '../features/companies/components';
-import { companiesLoader, companyLoader } from '../features/companies/loaders';
-import {TestArea} from '../pages/testarea';
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
+import { App } from ".";
+import { Login } from "../pages/login";
+import { requireAuthLoader } from "../features/auth/loaders";
+import { Companies, Company } from "../features/companies/components";
+import { companiesLoader, companyLoader } from "../features/companies/loaders";
+import { TestArea } from "../pages/testarea";
+import { ModuleList } from "../features/shared/components";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +26,8 @@ export const router = createBrowserRouter(
       <Route element={<Login />} path="/login" />
       {/* TODO: remove this in a production enviroment TestArea */}
       <Route element={<TestArea />} path="/testarea" />
+      {/* TODO: remove this in a production enviroment ModuleList, only here temporarily. */}
+      <Route element={<ModuleList />} path="/modulelist" />
     </>
   )
 );
