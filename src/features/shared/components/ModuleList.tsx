20ee0875be8cec92ee81af5da0752ff70bfe5267
Bosseyang <bosse_yang@hotmail.com>
2025-09-24 11:10:27 +0200

    changes regarding variable name change, icon wrapper,  openModules and comment updet

--- src/features/shared/components/ModuleList.tsx
+++ src/features/shared/components/ModuleList.tsx
@@ -29,10 +29,11 @@ export const ModuleList: React.FC<IModuleListProps> = ({ modules }) => {
                 className="module-button-item list-group-item"
               >
                 <div className="module-item-container">
-                  {}
-                  <span className="material-symbols-outlined">menu_book</span>
+                  <div className="icon-wrapper">
+                    <span className="material-symbols-outlined ">menu_book</span>
+                  </div>
                   <div className="title-date-container">
-                    <p className="module-title">{mod.title}</p>
+                    <p className="module-name">{mod.name}</p>
                     <div className="module-date-container">
                       <span className="material-symbols-outlined">calendar_today</span>
                       <p className="module-date">
@@ -41,22 +42,21 @@ export const ModuleList: React.FC<IModuleListProps> = ({ modules }) => {
                     </div>
                   </div>
                 </div>
-                {/* TODO: change these values to props where total= totalActivities and completed = completedActivities */}
+                {/* TODO: change these values to props where total= totalActivities and completed = completedActivities when Activities component and interface is implemented*/}
                 <div className="progress-arrow-container">
                   <ProgressBar total={4} completed={1} />
-                  {openModule === mod.id ? (
+                  {openModules.includes(mod.id) ? (
                     <span className="material-symbols-outlined">keyboard_arrow_down</span>
                   ) : (
                     <span className="material-symbols-outlined">keyboard_arrow_right</span>
                   )}
                 </div>
               </button>
-              {openModule === mod.id && (
-                <div className="border-t p-4 bg-gray-50">
+              {openModules.includes(mod.id) && (
+                <div className="activity-container">
                   {/* Placeholder for "Aktiviteter" */}
-                  <div className="text-gray-500 italic">
-                    Activity component goes here, this is just a placeholder.
-                  </div>
+                  <p>{mod.description}</p>
+                  <p className="">Activity component goes here, this is just a placeholder.</p>
                 </div>
               )}
             </li>
