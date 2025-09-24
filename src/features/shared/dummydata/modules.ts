5f5a8c8505a69959b6e75e72047c70135b38f57a
Bosseyang <bosse_yang@hotmail.com>
2025-09-24 11:05:06 +0200

    moved modules dummydata into its own folder

---
+++ src/features/shared/dummydata/modules.ts
@@ -0,0 +1,33 @@
+import { IModule } from "../types";
+
+// Dummy data, This is to be removed when backend is connected and implemented
+export const modules: IModule[] = [
+  {
+    id: "1",
+    name: "HTML & CSS",
+    description: "Grundläggande webbutveckling med HTML5 och CSS3",
+    startDate: "2023-08-15",
+    endDate: "2023-09-01",
+  },
+  {
+    id: "2",
+    name: "JavaScript",
+    description: "Grunderna i JavaScript programmering",
+    startDate: "2023-09-02",
+    endDate: "2023-09-20",
+  },
+  {
+    id: "3",
+    name: "React",
+    description: "Utveckling av moderna webbapplikationer med React",
+    startDate: "2023-09-21",
+    endDate: "2023-10-15",
+  },
+  {
+    id: "4",
+    name: "Databasedesign",
+    description: "Grundläggande databasdesign och SQL",
+    startDate: "2023-10-16",
+    endDate: "2023-11-05",
+  },
+];
