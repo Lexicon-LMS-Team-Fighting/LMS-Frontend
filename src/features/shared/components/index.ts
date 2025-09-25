5b31601c36b6f2bc921baebcc07875bcba7e5f40
Bosseyang <bosse_yang@hotmail.com>
2025-09-25 16:57:04 +0200

    add ActivityList and ActivitiesForModule as export

--- src/features/shared/components/index.ts
+++ src/features/shared/components/index.ts
@@ -2,3 +2,5 @@ export * from "./HeaderNav";
 export * from "./IconButton";
 export * from "./ModuleList";
 export * from "./ProgressBar";
+export * from "./ActivityList";
+export * from "./ActivitiesForModule";
