52399768016ed11303c4e221dfc7614db3ab7fbb
Bosseyang <bosse_yang@hotmail.com>
2025-09-25 16:49:09 +0200

    Create ActivityList component for the UI
    This component should work with the backend data fetch correctly. It is independent so it's plug and play incase it is needed somewhere else.
    
    TODO: The document link needs to be properly implemented when the backend for document is fixed.

---
+++ src/features/shared/components/ActivityList.tsx
@@ -0,0 +1,47 @@
+import { IActivity } from "../types";
+import { TitleDate } from "./TitleDate";
+
+interface IActivityListProps {
+  activities: IActivity[];
+}
+
+export const ActivityList: React.FC<IActivityListProps> = ({ activities }) => {
+  return (
+    <section className="activity-list-section">
+      <h3 className="activity-title">Aktiviteter</h3>
+      <ul className="list-group activity-list-group">
+        {activities?.map((activity) => (
+          <li key={activity.id} className="activity-list-item list-group">
+            <TitleDate
+              title={activity.name}
+              startDate={activity.startDate}
+              activityType={activity.activityType}
+            />
+
+            <div className="document-status-container">
+              {/* TODO: This path should be changed when document is implemented in backend */}
+              <a href="#" className="document-container">
+                <span className="material-symbols-outlined">news</span>Dokument
+              </a>
+
+              {activity.status && (
+                <span
+                  className={`status-container ${
+                    activity.status === "Försenad" ? "status-red" : "status-green"
+                  }`}
+                >
+                  {activity.status === "Försenad" ? (
+                    <span className="material-symbols-outlined">error</span>
+                  ) : (
+                    <span className="material-symbols-outlined">task_alt</span>
+                  )}
+                  {activity.status}
+                </span>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
