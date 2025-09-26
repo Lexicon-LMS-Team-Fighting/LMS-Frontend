bd62398243e3c780d560d37d5849b53f1ec4da8a
Bosseyang <bosse_yang@hotmail.com>
2025-09-26 11:01:18 +0200

    add feedback, the component should now show feedback only if there is feedback.

--- src/features/shared/components/ActivityList.tsx
+++ src/features/shared/components/ActivityList.tsx
@@ -18,29 +18,36 @@ export const ActivityList: React.FC<IActivityListProps> = ({ activities }) => {
               activityType={activity.activityType}
             />
 
-            <div className="document-status-container">
-              {/* TODO: This should be correctly implemented once the backend part for handling documents is in place */}
-              {
-                <a href="" className="document-container">
-                  <span className="material-symbols-outlined">news</span>Dokument
-                </a>
-              }
+              <div className="document-status-container">
+                {/* TODO: This should be correctly implemented once the backend part for handling documents is in place */}
+                {
+                  <a href="" className="document-container">
+                    <span className="material-symbols-outlined">news</span>Dokument
+                  </a>
+                }
 
-              {activity.status && (
-                <span
-                  className={`status-container ${
-                    activity.status === "Försenad" ? "status-red" : "status-green"
-                  }`}
-                >
-                  {activity.status === "Försenad" ? (
-                    <span className="material-symbols-outlined">error</span>
-                  ) : (
-                    <span className="material-symbols-outlined">task_alt</span>
-                  )}
-                  {activity.status}
-                </span>
-              )}
+                {activity.status && (
+                  <span
+                    className={`status-container ${
+                      activity.status === "Försenad" ? "status-red" : "status-green"
+                    }`}
+                  >
+                    {activity.status === "Försenad" ? (
+                      <span className="material-symbols-outlined">error</span>
+                    ) : (
+                      <span className="material-symbols-outlined">task_alt</span>
+                    )}
+                    {activity.status}
+                  </span>
+                )}
+              </div>
             </div>
+            {activity.feedback && (
+              <div className="feedback-container">
+                <strong>Feedback: </strong> <p>{activity.feedback}</p>
+              </div>
+            )}
+
           </li>
         ))}
       </ul>
