d9cc6f198d43a76f3a935e8daefb5adb188bafc5
jimmy-codix <212524470+jimmy-codix@users.noreply.github.com>
2025-09-26 07:35:50 +0200

    Add sidebar to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
 import { HeaderNav } from '../features/shared/components/HeaderNav';
+import { SideBar } from '../features/shared/components/SideBar';
 
 export function App() {
   return (
     <>
       <HeaderNav />
+      <SideBar />
       <Outlet />
     </>
   );
