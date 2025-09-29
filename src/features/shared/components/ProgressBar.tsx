3811002dfc2aa97eec7ad2ccde2b40da5bb54ec2
Bosseyang <bosse_yang@hotmail.com>
2025-09-29 14:11:23 +0200

    update to be re-usable for MyCourse component, added a fullsize version

--- src/features/shared/components/ProgressBar.tsx
+++ src/features/shared/components/ProgressBar.tsx
@@ -1,10 +1,18 @@
+//This progress bar can be universally used, it currently supports a fixed size use, for ModuleList component and a fullsize use for the MyCourse page component. It is in fixed size by defaule, set fullSize={true} to use the full size variant of progress bar.
+
 interface ProgressBarProps {
   total: number;
   completed: number;
   onComplete?: () => void;
+  fullSize?: boolean;
 }
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({ total, completed, onComplete }) => {
+export const ProgressBar: React.FC<ProgressBarProps> = ({
+  total,
+  completed,
+  onComplete,
+  fullSize,
+}) => {
   const percentage = total > 0 ? Math.round((completed / total) * 100) : 0;
 
   // Notify parent if completed through callback
@@ -13,10 +21,12 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({ total, completed, onCo
   }
 
   return (
-    <div className="progress-bar-container">
-      <span className="progress-bar-text">{percentage}% avklarat</span>
-      <div className="progress-bar">
-        <div className="current-progress" style={{ width: `${percentage}%` }} />
+    <div className={`progress-bar-container ${fullSize ? "full-size" : ""}`}>
+      <span className="progress-bar-text">
+        {percentage}% <p>avklarat</p>
+      </span>
+      <div className={`progress-bar`}>
+        <div className={`current-progress`} style={{ width: `${percentage}%` }} />
       </div>
     </div>
   );
