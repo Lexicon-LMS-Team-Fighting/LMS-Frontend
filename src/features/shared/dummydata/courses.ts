346336bc3335a2dfe30ee4d095191c39adabbfb1
Bosseyang <bosse_yang@hotmail.com>
2025-09-29 14:14:02 +0200

    add courses dummy data.

---
+++ src/features/shared/dummydata/courses.ts
@@ -0,0 +1,10 @@
+import { ICourse } from "../types";
+
+export const course: ICourse = {
+  id: "1",
+  name: "Frontend-utvecklare",
+  description:
+    "En omfattande utbildning i modern webbutveckling med fokus på JavaScript och React.",
+  startDate: "2023-08-15",
+  endDate: "2023-09-01",
+};
