7e3c75c39826e90a91661a04a8a5c06bcd644505
a21jeaha <hisui86@gmail.com>
2025-09-30 13:47:09 +0200

    decodes claim with correct key

--- src/features/shared/utilities/jwtDecoder.ts
+++ src/features/shared/utilities/jwtDecoder.ts
@@ -1,30 +1,20 @@
-import { jwtDecode, JwtPayload } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { getTokens } from "../../auth/utilities";
 
-interface AppJwtClaims {
-  name: string;
-  guid: string;
-  role: string;
-  expiration: number;
-  // FirstName: string;
-  // LastName: string;
-}
-
-export function getCurrentUserClaims(): AppJwtClaims | null {
+export function getCurrentUserId(): string | null {
   const token = getTokens();
 
   if (!token) return null;
 
   try {
-    return jwtDecode<AppJwtClaims>(token.accessToken);
+    const claim: any = jwtDecode(token.accessToken);
+
+    return (
+      claim[
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+      ] ?? null
+    );
   } catch {
     return null;
   }
-}
-
-export function getCurrentUserId(): string | null {
-  const claim = getCurrentUserClaims();
-
-  /*["guid"]*/
-  return claim?.guid ?? null;
 }
