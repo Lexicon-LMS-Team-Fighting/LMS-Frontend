89fecdd8fefeee2b1a0ae3881306485574218c53
a21jeaha <hisui86@gmail.com>
2025-09-30 13:47:51 +0200

    successfull login now rerouts to dashboard

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -12,29 +12,40 @@ import { TestArea } from "../pages/testarea";
 import Dashboard from "../pages/Dashboard";
 import { ModuleList } from "../features/shared/components";
 import { modules } from "../features/shared/dummydata";
+import { DashboardDifferedLoader } from "../features/auth/loaders/courseLoader";
+import RoleSwitch from "../pages/RoleSwitch";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       {/* requireAuthLoader is a route guard that protects the App and its child routes. */}
       {/* TODO uncomment line below and remove "<Route element={<App />} path="/"></Route>" when front end login is done */}
-      <Route element={<App />} loader={requireAuthLoader} path="/">
-        {/*    <Route element={<App />} path="/"> */}
-        {/*         <Route element={<Companies />} index loader={companiesLoader} />
-        <Route
-          element={<Company />}
-          loader={({ params }) => {
-            return companyLoader(params.id);
-          }}
-          path="companies/:id"
-        /> */}
-      </Route>
       <Route element={<Login />} path="/login" />
-      <Route element={<Dashboard />} path="/dashboard" />
-      {/* TODO: remove this in a production enviroment TestArea */}
-      <Route element={<TestArea />} path="/testarea" />
-      {/* TODO: remove this in a production enviroment ModuleList, only here temporarily. */}
-      <Route element={<ModuleList modules={modules} />} path="/modulelist" />
+      <Route element={<App />} loader={requireAuthLoader} path="/">
+        <Route
+          element={<RoleSwitch />}
+          path="dashboard"
+          loader={DashboardDifferedLoader}
+        />
+        {/* TODO: remove this in a production enviroment TestArea */}
+        <Route element={<TestArea />} path="/testarea" />
+        {/* TODO: remove this in a production enviroment ModuleList, only here temporarily. */}
+        <Route element={<ModuleList modules={modules} />} path="/modulelist" />
+      </Route>
     </>
   )
 );
+
+{
+  /*    <Route element={<App />} path="/"> */
+}
+{
+  /*         <Route element={<Companies />} index loader={companiesLoader} />
+<Route
+  element={<Company />}
+  loader={({ params }) => {
+    return companyLoader(params.id);
+  }}
+  path="companies/:id"
+/> */
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,19 @@
-import { ReactElement } from 'react';
-import TeacherDashboard from '../features/shared/components/TeacherDashboard';
-import StudentDashboard from '../features/shared/components/StudentDashboard';
-import "../features/shared/css/Dashboard.css"
+import { ReactElement } from "react";
+import TeacherDashboard from "../features/shared/components/TeacherDashboard";
+import StudentDashboard from "../features/shared/components/StudentDashboard";
+import "../features/shared/css/Dashboard.css";
+import { useLoaderData } from "react-router";
+import { ICourseDifferedLoaderReturn } from "../features/auth/loaders/courseLoader";
+import RoleSwitch from "./RoleSwitch";
 
+export default function Dashboard(): ReactElement {
+  const { userCourses } = useLoaderData<ICourseDifferedLoaderReturn>();
 
-export default function Dashboard(): ReactElement{
- 
-
-
+  console.log("");
   return (
-    <main className='main-container bg-gray-50'>
-        <TeacherDashboard />
+    <main className="main-container bg-gray-50">
+      {/* <TeacherDashboard /> */}
+      <RoleSwitch />
     </main>
   );
 }
