c79ab803f2a2821f200f34c8862ee7a4a5e21a48
a21jeaha <hisui86@gmail.com>
2025-09-30 09:21:39 +0200

    updated Router to route to Dashboard

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,15 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
-import { App } from '.';
-import { Login } from '../pages/login';
-import { requireAuthLoader } from '../features/auth/loaders';
-import { Companies, Company } from '../features/companies/components';
-import { companiesLoader, companyLoader } from '../features/companies/loaders';
-import {TestArea} from '../pages/testarea';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
+import { App } from ".";
+import { Login } from "../pages/login";
+import { requireAuthLoader } from "../features/auth/loaders";
+import { Companies, Company } from "../features/companies/components";
+import { companiesLoader, companyLoader } from "../features/companies/loaders";
+import { TestArea } from "../pages/testarea";
+import Dashboard from "../pages/Dashboard";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -12,8 +17,8 @@ export const router = createBrowserRouter(
       {/* requireAuthLoader is a route guard that protects the App and its child routes. */}
       {/* TODO uncomment line below and remove "<Route element={<App />} path="/"></Route>" when front end login is done */}
       <Route element={<App />} loader={requireAuthLoader} path="/">
-   {/*    <Route element={<App />} path="/"> */}
-{/*         <Route element={<Companies />} index loader={companiesLoader} />
+        {/*    <Route element={<App />} path="/"> */}
+        {/*         <Route element={<Companies />} index loader={companiesLoader} />
         <Route
           element={<Company />}
           loader={({ params }) => {
@@ -23,6 +28,7 @@ export const router = createBrowserRouter(
         /> */}
       </Route>
       <Route element={<Login />} path="/login" />
+      <Route element={<Dashboard />} path="/dashboard" />
       {/* TODO: remove this in a production enviroment TestArea */}
       <Route element={<TestArea />} path="/testarea" />
     </>
