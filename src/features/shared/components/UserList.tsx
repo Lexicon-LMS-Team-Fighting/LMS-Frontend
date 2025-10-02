b676f932c459c81ffb3f01afad85cbfe2e1a3044
Bosseyang <bosse_yang@hotmail.com>
2025-10-02 15:48:06 +0200

    Use UserInfo.tsx component to display/render the users.

--- src/features/shared/components/UserList.tsx
+++ src/features/shared/components/UserList.tsx
@@ -6,8 +6,30 @@ interface IUserListProps {
 }
 
 export const UserList: React.FC<IUserListProps> = ({ users }) => {
+  if (!users || users.length === 0) {
+    return <p>No users available</p>;
+  }
+
+  const fields = Object.keys(users[0]).filter((key) => key !== "userId");
+
   return (
     <section className="user-list-container">
+      <ul className="user-list-group list-group">
+        <li
+          className="user-list-header desktop-view"
+          style={{ ["--col-count" as any]: fields.length }}
+        >
+          {fields.map((head, i) => (
+            <p className="item-heading" key={i}>
+              {head}
+            </p>
+          ))}
+        </li>
+
+        {users.map((user) => {
+          return <UserInfo key={user.userId} user={user} fields={fields} />;
+        })}
+      </ul>
     </section>
   );
 };
