616301b424bb734ef6fbfc7b12063723d0237988
Philip Jansson <90768022+Gitphil93@users.noreply.github.com>
2025-10-02 16:40:59 +0200

    Fixed sidemenu
    The sidemenu now opens and closes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,12 @@ import RoleSwitch from '../pages/RoleSwitch';
 import Menu from '../features/shared/components/Menu';
 
 export function App() {
+
   return (
     <>
       <HeaderNav />
       <div className="master-row">
-      <Menu />
+      <Menu  />
       <Outlet />
 
       {/* used to chose compontents based on role */}
