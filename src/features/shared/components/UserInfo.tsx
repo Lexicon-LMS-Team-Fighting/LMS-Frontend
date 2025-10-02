8282116c3fbc0b3a386a46892f1f1e0cb26f3822
Bosseyang <bosse_yang@hotmail.com>
2025-10-02 15:50:16 +0200

    Create UserInfo.tsx

---
+++ src/features/shared/components/UserInfo.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { IUser } from "../types";
+import { useMediaQuery } from "usehooks-ts";
+
+interface IUserInfoProps {
+  user: IUser;
+  fields: string[];
+}
+export const UserInfo: React.FC<IUserInfoProps> = ({ user, fields }) => {
+  return (
+    <>
+    </>
+  );
+};
