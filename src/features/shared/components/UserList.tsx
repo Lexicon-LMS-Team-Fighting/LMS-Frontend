3af8b11708217248eb61d4a79463112ddcae800a
Bosseyang <bosse_yang@hotmail.com>
2025-10-02 15:46:48 +0200

    move out to dummydata folder

--- src/features/shared/components/UserList.tsx
+++ src/features/shared/components/UserList.tsx
@@ -1,13 +1,5 @@
 import { IUser } from "../types";
-
-// TODO: Remove temporary dummy data
-export const users = [
-  { userId: "1", userName: "anna01", name: "Anna Andersson", email: "anna.andersson@example.com" },
-  { userId: "2", userName: "erikE", name: "Erik Eriksson", email: "erik.eriksson@example.com" },
-  { userId: "3", userName: "mariaS", name: "Maria Svensson", email: "maria.svensson@example.com" },
-  { userId: "4", userName: "johanN", name: "Johan Nilsson", email: "johan.nilsson@example.com" },
-  { userId: "5", userName: "saraL", name: "Sara Lindberg", email: "sara.lindberg@example.com" },
-];
+import { UserInfo } from "./UserInfo";
 
 interface IUserListProps {
   users: IUser[];
