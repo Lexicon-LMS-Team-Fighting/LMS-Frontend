311a3e533817de7766c15d8c8129387e014420e7
Philip Jansson <90768022+Gitphil93@users.noreply.github.com>
2025-10-03 09:38:38 +0200

    Updated route to /dashboard

--- src/features/shared/components/Menu.tsx
+++ src/features/shared/components/Menu.tsx
@@ -30,7 +30,7 @@ export default function Menu(): ReactElement{
       </button>
 
           <div className="menu-inner">
-            <Link to="/" className={`menu-item ${isTabActive("/", true) ? "active" : ""}`}>
+            <Link to="/dashboard" className={`menu-item ${isTabActive("/dashboard", true) ? "active" : ""}`}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="" aria-hidden="true" data-id="element-32"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z">
                 </path></svg>
 
