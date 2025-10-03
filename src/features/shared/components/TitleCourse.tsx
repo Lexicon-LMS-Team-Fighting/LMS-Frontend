f25d2d346a865055439f5f724f516df902d4f4c4
jimmy-codix <212524470+jimmy-codix@users.noreply.github.com>
2025-10-03 13:34:14 +0200

    Removed unused code

--- src/features/shared/components/TitleCourse.tsx
+++ src/features/shared/components/TitleCourse.tsx
@@ -1,6 +1,10 @@
 //A container component showing title, date and courseName if available
 //This component is based of the TitleDate component
 
+//TODO rename classNames so they won't conflict with TitleDate.css
+import React from "react";
+import "../css/TitleCourse.css";
+
 interface ICourseDateProps {
   title?: string;
   startDate: string;
@@ -15,19 +19,21 @@ export const TitleCourse: React.FC<ICourseDateProps> = ({
   courseName,
 }) => {
   return (
-    <div className={activityType ? `titledate-container-activity` : "titledate-container"}>
+    <div className="titledate-container-activity">
+      {/* Add title if available */}
       {title && (
         <div className="title-container">
           {activityType && <span className="activity-wrapper">{activityType}</span>}
           <p className="titledate-title">{title}</p>
         </div>
       )}
+      {/* Add date or courseName if available */}
       <div className="date-container">
-        {!courseName ? (
+        {!courseName && (
           <span className="material-symbols-outlined hidden">calendar_today</span>
-        ) : null}
-        
-          <p className="titledate-date">{!courseName? (startDate) : (courseName)}</p>
+        )}
+          
+        <p className="titledate-date">{!courseName? (startDate) : (courseName)}</p>
       </div>
     </div>
   );
