217bfbca625a973b763f6110a2e121e445ef1b45
Bosseyang <bosse_yang@hotmail.com>
2025-10-06 15:38:21 +0200

    implement loader into participants page to load the data from backend.

--- src/pages/courseparticipant/CourseParticipants.tsx
+++ src/pages/courseparticipant/CourseParticipants.tsx
@@ -1,11 +1,14 @@
+import { useLoaderData } from "react-router";
 import { UserList } from "../../features/shared/components/UserList";
-import { users } from "../../features/shared/dummydata";
+import { IUserParticipants } from "../../features/shared/types";
 
 export const CourseParticipants: React.FC = () => {
+  const participants = useLoaderData() as IUserParticipants[];
+
   return (
     <main className="course-participants-page">
       <h1 className="course-participants-title">Kursdeltagare</h1>
-      <UserList users={users} />
+      <UserList users={participants} />
     </main>
   );
 };
