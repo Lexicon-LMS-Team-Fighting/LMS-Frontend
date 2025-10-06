803e163fb919521d37e5e22808b880bb6a1745c9
Bosseyang <bosse_yang@hotmail.com>
2025-10-06 15:30:56 +0200

    add exception for status code 400

--- src/fetchers/participantsFetcher.ts
+++ src/fetchers/participantsFetcher.ts
@@ -6,7 +6,10 @@ interface IPagedResult<T> {
   items: T[];
   metadata: any;
 }
+
 export async function fetchParticipants(courseId: string): Promise<IUserParticipants[]> {
+  if (!courseId) throw new Response("Course id missing", { status: 400 });
+
   const data: IPagedResult<IUser> = await fetchWithToken<IPagedResult<IUser>>(
     `${BASE_URL}/course/${courseId}/participants`
   );
