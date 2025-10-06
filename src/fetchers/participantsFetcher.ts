99f184f7ebce156573efef5d45636c31c97f60a8
Bosseyang <bosse_yang@hotmail.com>
2025-10-06 15:29:54 +0200

    fetch participants using fetchWithToken

--- src/fetchers/participantsFetcher.ts
+++ src/fetchers/participantsFetcher.ts
@@ -1,3 +1,11 @@
 import { IUser, IUserParticipants } from "../features/shared/types";
+import { BASE_URL } from "../features/shared/constants";
+interface IPagedResult<T> {
+  items: T[];
+  metadata: any;
+}
 export async function fetchParticipants(courseId: string): Promise<IUserParticipants[]> {
+  const data: IPagedResult<IUser> = await fetchWithToken<IPagedResult<IUser>>(
+    `${BASE_URL}/course/${courseId}/participants`
+  );
 }
