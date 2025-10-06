b802b4d827947a657cf81d7482e13e37bcad0c98
Bosseyang <bosse_yang@hotmail.com>
2025-10-06 15:36:58 +0200

    fetch participants using fetchParticipants if user is registered to any course.

--- src/features/auth/loaders/participantsLoader.ts
+++ src/features/auth/loaders/participantsLoader.ts
@@ -1,6 +1,12 @@
+import { fetchParticipants } from "../../../fetchers/participantsFetcher";
 import { fetchUserFromToken } from "../../../fetchers/userFetcher";
 import { IUserExtended, IUserParticipants } from "../../shared/types";
 export async function participantsLoader(): Promise<IUserParticipants[]> {
   const user: IUserExtended = await fetchUserFromToken();
+
+  if (user.courses.length > 0) {
+    return fetchParticipants(user.courses[0].id);
+  }
+
   return [];
 }
