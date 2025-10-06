d27bb66a7155dee6f4de570976611036ae93a258
Bosseyang <bosse_yang@hotmail.com>
2025-10-06 15:36:08 +0200

    fetch user using fetchUserFromToken

--- src/features/auth/loaders/participantsLoader.ts
+++ src/features/auth/loaders/participantsLoader.ts
@@ -1,4 +1,6 @@
+import { fetchUserFromToken } from "../../../fetchers/userFetcher";
 import { IUserExtended, IUserParticipants } from "../../shared/types";
 export async function participantsLoader(): Promise<IUserParticipants[]> {
+  const user: IUserExtended = await fetchUserFromToken();
   return [];
 }
