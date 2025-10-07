ceb88d48dd6683aee739d08a1a677493ea9e32af
jimmy-codix <212524470+jimmy-codix@users.noreply.github.com>
2025-10-07 18:31:48 +0200

    Renamed classes so it wont conflict with TitleDate component

--- src/features/shared/components/TitleCourse.tsx
+++ src/features/shared/components/TitleCourse.tsx
@@ -1,39 +1,38 @@
 //A container component showing title, date and courseName if available
 //This component is based of the TitleDate component
 
-//TODO rename classNames so they won't conflict with TitleDate.css
 import React from "react";
 import "../css/TitleCourse.css";
 
-interface ICourseDateProps {
+interface ITitelCourseProps {
   title?: string;
   startDate: string;
   activityType?: string;
   courseName?: string;
 }
 
-export const TitleCourse: React.FC<ICourseDateProps> = ({
+export const TitleCourse: React.FC<ITitelCourseProps> = ({
   title,
   startDate,
   activityType,
   courseName,
 }) => {
   return (
-    <div className="titledate-container-activity">
+    <div className="titlecourse-container-activity">
       {/* Add title if available */}
       {title && (
         <div className="title-container">
           {activityType && <span className="activity-wrapper">{activityType}</span>}
-          <p className="titledate-title">{title}</p>
+          <p className="titlecourse-title">{title}</p>
         </div>
       )}
       {/* Add date or courseName if available */}
-      <div className="date-container">
+      <div className="info-container">
         {!courseName && (
           <span className="material-symbols-outlined hidden">calendar_today</span>
         )}
           
-        <p className="titledate-date">{!courseName? (startDate) : (courseName)}</p>
+        <p className="titlecourse-date">{!courseName? (startDate) : (courseName)}</p>
       </div>
     </div>
   );
