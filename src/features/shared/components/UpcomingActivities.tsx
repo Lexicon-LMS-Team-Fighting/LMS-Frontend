5768578d8af847075f22fcaaa095ca65cfc8f538
jimmy-codix <212524470+jimmy-codix@users.noreply.github.com>
2025-10-07 14:04:04 +0200

    Removed unused code

--- src/features/shared/components/UpcomingActivities.tsx
+++ src/features/shared/components/UpcomingActivities.tsx
@@ -1,39 +1,30 @@
-// import { useState } from "react";
-import { IActivity, IUpcomingActivity } from "../types";
+import { IUpcomingActivity } from "../types";
 import { TitleCourse } from "./TitleCourse";
 import "../css/UpcomingActivities.css";
 
-//TODO fix this later when activities are being fetched from backend
 interface IActivityListProps {
   activities: IUpcomingActivity[];
   count: number;
 }
 
 export const UpcomingActivities: React.FC<IActivityListProps> = ({ activities, count }) => {
-  //console.log("Activities in UpcomingActivities", activities);
-  // const activities = devPops;
-  // const [expandedActivities, setExpandedActivities] = useState<string[]>([]);
 
-  const handleClick = (activity: IActivity) => {
-    if (!activity.description) return;
 
-    setExpandedActivities((prev) =>
-      prev.includes(activity.id) ? prev.filter((id) => id !== activity.id) : [...prev, activity.id]
-    );
-  };
+  if (!activities || activities.length === 0) {
+    return <p>Inga kommande aktiviteter</p>;
+  }
 
+  console.log("Activities in UpcomingActivities component:", activities);
   const act = activities.slice(0, count); // Show only the first 5 activities
 
   return (
     <section className="activity-list-section">
       <ul className="list-group activity-list-group">
         {act?.map((activity) => {
-          console.log("Activity:", activity.startDate);
           return (
             <li
               key={activity.id}
               className={`activity-list-item list-group ${activity.description ? "clickable" : ""}`}
-              onClick={() => handleClick(activity)}
             >
               <div className="activity-list-box">
                 <TitleCourse
