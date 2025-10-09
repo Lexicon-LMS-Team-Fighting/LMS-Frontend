43167df827ec790fee624b5b5f94a1ee4615d863
Bosseyang <bosse_yang@hotmail.com>
2025-10-09 09:48:35 +0200

    Increase pageSize to include all modules for now

--- src/fetchers/modulesFetcher.ts
+++ src/fetchers/modulesFetcher.ts
@@ -3,16 +3,23 @@ import { IModulePreview, IPaginatedResponse } from "../features/shared/types";
 import { fetchWithToken } from "../features/shared/utilities/fetchWithToken";
 import { catchFetchErrors } from "./fetchErrorsCatcher";
 
-export async function fetchModulesByCourseId(guid: string, pageNumber: number = 1, pageSize: number = 10): Promise<IPaginatedResponse<IModulePreview>> {
+export async function fetchModulesByCourseId(
+  guid: string,
+  pageNumber: number = 1,
+  pageSize: number = 10000
+): Promise<IPaginatedResponse<IModulePreview>> {
   try {
-    const response = await fetchWithToken<IPaginatedResponse<IModulePreview>>(`${BASE_URL}/course/${guid}/modules?page=${pageNumber}&pageSize=${pageSize}&include=progress`, {});
+    const response = await fetchWithToken<IPaginatedResponse<IModulePreview>>(
+      `${BASE_URL}/course/${guid}/modules?page=${pageNumber}&pageSize=${pageSize}&include=progress`,
+      {}
+    );
 
     const modulesWithDates = response.items.map((m) => ({
       ...m,
       startDate: new Date(m.startDate),
       endDate: new Date(m.endDate),
     }));
-    
+
     return {
       ...response,
       items: modulesWithDates,
